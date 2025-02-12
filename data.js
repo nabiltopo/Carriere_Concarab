548aec73ed2c3343c2d78dd3cb424e8f62cc4bc1
nabiltopo1 <nabil.boutrik1@gmail.com>
2025-02-13 00:16:26 +0100

    Commit

---
+++ data.js
@@ -0,0 +1,281 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dji_20250120133244_0001_v",
+      "name": "DJI_20250120133244_0001_V",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7180943349526174,
+          "pitch": 0.10695228367431042,
+          "rotation": 0,
+          "target": "3-dji_20250120133742_0004_v"
+        },
+        {
+          "yaw": 1.7307456113257293,
+          "pitch": 0.059281146316770617,
+          "rotation": 0,
+          "target": "4-dji_20250120133914_0005_v"
+        },
+        {
+          "yaw": -0.21170275451501297,
+          "pitch": 0.09359773883873146,
+          "rotation": 0,
+          "target": "2-dji_20250120133547_0003_v"
+        },
+        {
+          "yaw": -1.50726409627406,
+          "pitch": -0.10623921132349068,
+          "rotation": 0,
+          "target": "1-dji_20250120133416_0002_v"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-dji_20250120133416_0002_v",
+      "name": "DJI_20250120133416_0002_V",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.945270900070831,
+          "pitch": 0.03982065215392794,
+          "rotation": 2.356194490192345,
+          "target": "0-dji_20250120133244_0001_v"
+        },
+        {
+          "yaw": 1.7332954311240965,
+          "pitch": 0.0810537330874137,
+          "rotation": 3.141592653589793,
+          "target": "2-dji_20250120133547_0003_v"
+        },
+        {
+          "yaw": 2.3147653508812764,
+          "pitch": 0.03170423559525659,
+          "rotation": 3.141592653589793,
+          "target": "3-dji_20250120133742_0004_v"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-dji_20250120133547_0003_v",
+      "name": "DJI_20250120133547_0003_V",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8363074290986905,
+          "pitch": 0.11998802328514913,
+          "rotation": 2.356194490192345,
+          "target": "1-dji_20250120133416_0002_v"
+        },
+        {
+          "yaw": 1.605237398868324,
+          "pitch": 0.09646839641734672,
+          "rotation": 3.141592653589793,
+          "target": "0-dji_20250120133244_0001_v"
+        },
+        {
+          "yaw": 0.485760429623058,
+          "pitch": 0.04778753120274715,
+          "rotation": 2.356194490192345,
+          "target": "3-dji_20250120133742_0004_v"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-dji_20250120133742_0004_v",
+      "name": "DJI_20250120133742_0004_V",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": 1.0348699541754787,
+        "pitch": 0.915014867053161,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.295628739648035,
+          "pitch": 0.022312729016293886,
+          "rotation": 0,
+          "target": "0-dji_20250120133244_0001_v"
+        },
+        {
+          "yaw": 2.825720692168785,
+          "pitch": -0.07493991557739399,
+          "rotation": 2.356194490192345,
+          "target": "1-dji_20250120133416_0002_v"
+        },
+        {
+          "yaw": -2.9674875547282227,
+          "pitch": -0.08704136132471696,
+          "rotation": 3.141592653589793,
+          "target": "2-dji_20250120133547_0003_v"
+        },
+        {
+          "yaw": 1.2491720383305331,
+          "pitch": 0.063075639487149,
+          "rotation": 2.356194490192345,
+          "target": "4-dji_20250120133914_0005_v"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4429334554628923,
+          "pitch": 0.5081964853669785,
+          "title": "Carrière Concarab",
+          "text": "Driouch"
+        }
+      ]
+    },
+    {
+      "id": "4-dji_20250120133914_0005_v",
+      "name": "DJI_20250120133914_0005_V",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Carrière Concarab",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
